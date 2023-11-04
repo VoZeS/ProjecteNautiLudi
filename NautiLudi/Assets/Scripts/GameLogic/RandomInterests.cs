162281d74e8d3db8a1ef6415b20022d637900dbc
VoZeS <davidboces12@hotmail.com>
2023-11-04 17:23:29 +0100

    NewsUI Beggining

--- NautiLudi/Assets/Scripts/GameLogic/RandomInterests.cs
+++ NautiLudi/Assets/Scripts/GameLogic/RandomInterests.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RandomInterests : MonoBehaviour
 {
     // SLIDERS
+    [Header("Interests")]
     public Slider socialInterestSlider;
     public Slider sportsInterestSlider;
     public Slider internationalInterestSlider;
 
+    [Header("Days")]
+    public int dayCount = 1;
+    public TMP_Text dayText;
+
     private void Start()
     {
-        RandInterests();
+        // Initial values
+        socialInterestSlider.value = 0.5f;
+        sportsInterestSlider.value = 0.5f;
+        internationalInterestSlider.value = 0.5f;
+
+        dayCount = 1;
+    }
+
+    private void Update()
+    {
+        dayText.text = "Day " + dayCount;
     }
 
     public void RandInterests()
@@ -20,5 +36,6 @@ public class RandomInterests : MonoBehaviour
         socialInterestSlider.value = Random.Range(0.0f, 1.0f);
         sportsInterestSlider.value = Random.Range(0.0f, 1.0f);
         internationalInterestSlider.value = Random.Range(0.0f, 1.0f);
+
     }
 }
