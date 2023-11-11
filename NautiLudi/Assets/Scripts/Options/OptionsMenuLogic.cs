4e6c4fe6ac5a66cde0052a273b729142ffe247e4
VoZeS <davidboces12@hotmail.com>
2023-11-11 13:41:20 +0100

    2 options button done

--- NautiLudi/Assets/Scripts/Options/OptionsMenuLogic.cs
+++ NautiLudi/Assets/Scripts/Options/OptionsMenuLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class OptionsMenuLogic : MonoBehaviour
 {
@@ -11,10 +12,15 @@ public class OptionsMenuLogic : MonoBehaviour
 
     // Set to true GO
     public GameObject optionsGroup;
+    public GameObject optionsMainMenu;
+    public GameObject optionsGameplay;
 
 
     // Between Scenes Logic
     private static bool optionsCreated = false;
+    private ChangeScene sceneScript;
+
+    public Button goToMainMenuButton;
 
     private void Awake()
     {
@@ -38,29 +44,101 @@ public class OptionsMenuLogic : MonoBehaviour
             Debug.Log("El GameObject mainMenuGroup no se ha asignado. Asigna el GameObject en el editor de Unity.");
             return;
         }
+
+        GameObject gameLogicManager = GameObject.Find("GameLogicManager");
+        if (gameLogicManager == null)
+        {
+            Debug.Log("El GameObject GameLogicManager no se ha encontrado. Asegúrate de que el objeto existe en la escena.");
+            return;
+        }
+
+        sceneScript = gameLogicManager.GetComponent<ChangeScene>();
+
+        if (sceneScript == null)
+        {
+            Debug.Log("El componente ChangeScene no se ha encontrado en el objeto GameLogicManager.");
+            return;
+        }
+
+
+        goToMainMenuButton.onClick.AddListener(GoToMainMenu);
+    }
+
+    public void GoToMainMenu()
+    {
+        sceneScript.GoToScene("MainMenuScene");
     }
 
     public void OpenOptionsMenu()
     {
-        if(mainMenuGroup == null)
-            mainMenuGroup = GameObject.Find("_MainMenu");
+        if(SceneManager.GetActiveScene().name == "MainMenuScene")
+        {
+            if (mainMenuGroup == null)
+                mainMenuGroup = GameObject.Find("_MainMenu");
 
-        //if (mainMenuGroup != null)
-        //    mainMenuGroup.SetActive(false);
+            if (optionsGroup != null)
+            {
+                optionsGroup.SetActive(true);
 
-        if (optionsGroup != null)
-            optionsGroup.SetActive(true);
+                if (optionsMainMenu != null)
+                {
+                    optionsMainMenu.SetActive(true);
+                    optionsGameplay.SetActive(false);
+                }
+
+            }
+        }
+        else if(SceneManager.GetActiveScene().name == "GameplayScene")
+        {
+            if (optionsGroup != null)
+            {
+                optionsGroup.SetActive(true);
+
+                if (optionsGameplay != null)
+                {
+                    optionsGameplay.SetActive(true);
+                    optionsMainMenu.SetActive(false);
+                }
+            }
+        }
+
     }
     public void CloseOptionsMenu()
     {
-        if (mainMenuGroup == null)
-            mainMenuGroup = GameObject.Find("_MainMenu");
+        if (SceneManager.GetActiveScene().name == "MainMenuScene")
+        {
+            if (mainMenuGroup == null)
+                mainMenuGroup = GameObject.Find("_MainMenu");
 
-        if (mainMenuGroup != null)
-            mainMenuGroup.SetActive(true);
+            if (mainMenuGroup != null)
+                mainMenuGroup.SetActive(true);
 
-        if (optionsGroup != null)
-            optionsGroup.SetActive(false);
+            if (optionsGroup != null)
+            {
+                optionsGroup.SetActive(false);
+
+                if (optionsMainMenu != null)
+                {
+                    optionsMainMenu.SetActive(true);
+                    optionsGameplay.SetActive(false);
+                }
+
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "GameplayScene")
+        {
+            if (optionsGroup != null)
+            {
+                optionsGroup.SetActive(false);
+
+                if (optionsGameplay != null)
+                {
+                    optionsGameplay.SetActive(true);
+                    optionsMainMenu.SetActive(false);
+                }
+
+            }
+        }
     }
 
 }
