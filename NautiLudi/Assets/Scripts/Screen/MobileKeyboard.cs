9d68fa69475af47acae0c4cdc8692e20ae6dedc3
VoZeS <davidboces12@hotmail.com>
2023-11-15 11:40:20 +0100

    TMP

--- NautiLudi/Assets/Scripts/Screen/MobileKeyboard.cs
+++ NautiLudi/Assets/Scripts/Screen/MobileKeyboard.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MobileKeyboard : MonoBehaviour
 {
-    public InputField inputField;
+    public TMP_InputField inputField;
 
     public void OpenKeyboard()
     {
